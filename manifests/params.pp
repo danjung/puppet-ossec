c109f300762a40f16ff0d5b2f3b8d65503a23dcc
Kyle Campos <kyle.campos@gmail.com>
2013-11-06 10:32:49 -0700

    Add base hidspackage to params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,11 @@
 class ossec::params {
 	case $osfamily {
     'RedHat' : {
-      $hidsagentservice='ossec-hids-agent'
-      $hidsagentpackage='ossec-hids-agent'
-      $hidsserverservice='ossec-hids-server'
-      $hidsserverpackage='ossec-hids-server'
+      $hidspackage       = 'ossec-hids'
+      $hidsagentservice  = 'ossec-hids-agent'
+      $hidsagentpackage  = 'ossec-hids-agent'
+      $hidsserverservice = 'ossec-hids-server'
+      $hidsserverpackage = 'ossec-hids-server'
     }
   }
 }
