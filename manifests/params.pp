d789acc6d8bc41e37b35355a1ddb8e76af4b34fb
Kyle Campos <kyle.campos@gmail.com>
2013-11-21 12:42:32 -0700

    Added necessary keys to params

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,11 +1,13 @@
 class ossec::params {
-	case $osfamily {
+  case $osfamily {
     'RedHat' : {
+      $install_home      = '/var/ossec'
+      $group             = 'ossec'
       $hidspackage       = 'ossec-hids'
       $hidsagentservice  = 'ossec-hids-agent'
       $hidsagentpackage  = 'ossec-hids-agent'
       $hidsserverservice = 'ossec-control'
-      $hidsservicepath   = '/var/ossec/bin'
+      $hidsservicepath   = "${install_home}/bin"
       $hidsserverpackage = 'ossec-hids-server'
     }
   }
