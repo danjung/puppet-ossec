0197ca371e5629d688a4ff524a409189ec277c0f
Kyle Campos <kyle.campos@gmail.com>
2013-11-22 11:55:36 -0700

    Adjust post_install_workarounds to only run if file exists

--- manifests/post_install_workarounds.pp
+++ manifests/post_install_workarounds.pp
@@ -1,8 +1,7 @@
 class ossec::post_install_workarounds inherits ossec::params {
-	file { "${install_home}/etc/shared/ar.conf" :
-		ensure => file,
-		owner  => 'root',
-		group  => $group,
-		mode   => 0440
-	}
+  exec { "fix-ar-perms":
+    command => "chgrp ${group} ${install_home}/etc/shared/ar.conf",
+    path    => "/usr/bin:/usr/sbin:/bin:/usr/local/bin",
+    onlyif  => "test -f ${install_home}/etc/shared/ar.conf"
+  }
 }
